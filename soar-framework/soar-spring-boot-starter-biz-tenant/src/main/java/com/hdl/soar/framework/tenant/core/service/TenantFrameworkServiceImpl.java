a067979481c01363ae0974fcf73eee691f1b3627
long <haduylong109710@gmail.com>
2026-07-13 23:34:33 +0700

    fix: tenantApi may be uninitialize

--- soar-framework/soar-spring-boot-starter-biz-tenant/src/main/java/com/hdl/soar/framework/tenant/core/service/TenantFrameworkServiceImpl.java
+++ soar-framework/soar-spring-boot-starter-biz-tenant/src/main/java/com/hdl/soar/framework/tenant/core/service/TenantFrameworkServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hdl.soar.framework.tenant.core.service;
 
+import com.github.benmanes.caffeine.cache.CacheLoader;
 import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.hdl.soar.framework.common.biz.system.tenant.TenantCommonApi;
 import com.hdl.soar.framework.common.exception.ServiceException;
@@ -24,19 +25,27 @@ public class TenantFrameworkServiceImpl implements TenantFrameworkService {
      */
     private final LoadingCache<Object, List<Long>> getTenantIdsCache = CacheUtils.buildAsyncReloadingCaffeine(
             Duration.ofMinutes(1L), // Expiration time: 1 minute
-            key -> tenantApi.getTenantIdList());
+            new CacheLoader<>() {
+                @Override
+                public List<Long> load(Object key) {
+                    return tenantApi.getTenantIdList();
+                }
+            });
 
     /**
      * Cache for {@link #validTenant(Long)}.
      */
     private final LoadingCache<Long, ServiceException> validTenantCache = CacheUtils.buildAsyncReloadingCaffeine(
             Duration.ofMinutes(1L), // Expiration time: 1 minute
-            id -> {
-                try {
-                    tenantApi.validateTenant(id);
-                    return SERVICE_EXCEPTION_NULL;
-                } catch (ServiceException ex) {
-                    return ex;
+            new CacheLoader<>() {
+                @Override
+                public ServiceException load(Long id) {
+                    try {
+                        tenantApi.validateTenant(id);
+                        return SERVICE_EXCEPTION_NULL;
+                    } catch (ServiceException ex) {
+                        return ex;
+                    }
                 }
             });
 
